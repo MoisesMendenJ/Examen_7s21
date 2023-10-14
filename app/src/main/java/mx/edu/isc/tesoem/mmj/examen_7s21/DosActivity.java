b30f3fdb90bba67e4d1ab810a8bba9c2dcf09e90
Moises Mendez J <moikita_zmen156@hotmail.com>
2023-10-14 01:54:48 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/isc/tesoem/mmj/examen_7s21/DosActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.isc.tesoem.mmj.examen_7s21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dos);
+    }
+}
